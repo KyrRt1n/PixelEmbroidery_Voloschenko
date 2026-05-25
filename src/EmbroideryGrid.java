06271b3db4877fd9820dc77cc7874019ee33b6a5
Artem Voloschenko <avoloschenko008@gmail.com>
2026-05-25 14:49:20 +0300

    centering imported image

--- src/EmbroideryGrid.java
+++ src/EmbroideryGrid.java
@@ -15,12 +15,17 @@ public class EmbroideryGrid {
     }
 
     public void importFromImg(BufferedImage img){
-        int maxRows = Math.min(rows, img.getHeight());
-        int maxCols = Math.min(cols, img.getWidth());
+        ClearGrid();
+        int startRow = (rows - img.getHeight()) / 2;
+        int startCol = (cols - img.getWidth()) / 2;
 
-        for(int r = 0; r < maxRows; r++){
-            for(int c = 0; c < maxCols; c++) {
-                cells[r][c] = new Color(img.getRGB(c, r));
+        for(int r = 0; r < img.getHeight(); r++){
+            for(int c = 0; c < img.getWidth(); c++) {
+                int gridRow = startRow + r;
+                int gridCol = startCol + c;
+
+                if (gridRow >= 0 && gridRow < rows && gridCol >= 0 && gridCol < cols)
+                    cells[gridRow][gridCol] = new Color(img.getRGB(c, r));
             }
         }
     }
